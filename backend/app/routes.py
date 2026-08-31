c73a90c7b04e824de60e4ac0d2682aa1179e009d
MarriHarshaVardhan <harshavardhanmarri08@gmail.com>
2026-08-31 12:48:14 +0530

    modified routes.py

--- backend/app/routes.py
+++ backend/app/routes.py
@@ -1,11 +1,13 @@
 from fastapi import APIRouter
 
-from app.api.authentication import (router as authentication_router)
+from app.api.authentication import router as authentication_router
 from app.api.recent_screening import router as screening_router
 from app.api.screening_history import router as screening_history_router
+from app.api.resume_upload import router as resume_upload_router
 
 router = APIRouter()
 
 router.include_router(authentication_router)
+router.include_router(resume_upload_router)
 router.include_router(screening_router, prefix="/api")
 router.include_router(screening_history_router)
